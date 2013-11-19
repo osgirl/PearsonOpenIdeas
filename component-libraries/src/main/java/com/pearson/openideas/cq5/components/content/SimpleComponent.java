6224d34c246e2984ef8a989b0451b58c85617877
John Spyronis <jspyronis@tacitknowledge.com>
2013-11-19 17:35:48 +0000

    Same --- adding simple component to see how it will be wired up with CQ-components

--- component-libraries/src/main/java/com/pearson/openideas/cq5/components/content/SimpleComponent.java
+++ component-libraries/src/main/java/com/pearson/openideas/cq5/components/content/SimpleComponent.java
@@ -2,6 +2,8 @@ package com.pearson.openideas.cq5.components.content;
 
 import com.crownpartners.cq.quickstart.core.component.AbstractComponent;
 import com.day.cq.wcm.api.WCMMode;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.sling.settings.SlingSettingsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,8 +14,13 @@ import javax.servlet.jsp.PageContext;
  *         Date: 19/11/2013
  *         Time: 15:05
  */
+
 public class SimpleComponent extends AbstractComponent
 {
+    @Reference
+    private SlingSettingsService slingSettings;
+
+
     private static final Logger LOGGER = LoggerFactory.getLogger(SimpleComponent.class);
     private String stringWcmMode;
 
@@ -26,12 +33,31 @@ public class SimpleComponent extends AbstractComponent
         super(pageContext);
     }
 
+
     @Override
     public void init()
     {
+
         WCMMode mode = WCMMode.fromRequest(getSlingRequest());
-        stringWcmMode = mode.toString();
-        LOGGER.debug("The run mode is ::::::::::::::::::: " + mode);
+        //stringWcmMode = slingSettingsService.getRunModes().toString();
+
+        stringWcmMode = "test";
+
+//        // Get run mode of server
+//        final Set<String> runModes = slingSettings.getRunModes();
+//
+//        LOGGER.debug("----------------------------------------------------------------------------------------------------" + runModes);
+//
+//        for (String rm : runModes)
+//        {
+//            if (rm.equalsIgnoreCase("publish") || rm.equalsIgnoreCase("author"))
+//            {
+//                stringWcmMode = rm;
+//                break;
+//            }
+//        }
+
+        LOGGER.debug("The run mode is ::::::::::::::::::: " + stringWcmMode);
     }
 
     public String getStringWcmMode()
--- components/src/main/content/jcr_root/apps/plc/prkc/uk/open-ideas/components/content/simpleComponent/simpleComponent.jsp
+++ components/src/main/content/jcr_root/apps/plc/prkc/uk/open-ideas/components/content/simpleComponent/simpleComponent.jsp
@@ -1,6 +1,6 @@
 <%--
 
-  Related Articles component.
+  Simple component returning cq5 running mode.
 
   This component appears as a sidebar on the right side Article Template
 
@@ -9,10 +9,11 @@
 %><%@page session="false"%>
 <%@ page import="com.pearson.openideas.cq5.components.content.SimpleComponent" %>
 <%
-%><%
 
     SimpleComponent simpleComponent = new SimpleComponent(pageContext);
     pageContext.setAttribute("simpleComponent", simpleComponent);
+
+
 
 %>
 
